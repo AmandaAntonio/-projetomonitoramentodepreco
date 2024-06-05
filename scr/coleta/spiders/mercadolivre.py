39516143861cc78aa1690ab582876b2c0595f086
Amanda M Antonio <amanda.m.antonio@outlook.com.br>
2024-06-05 11:15:48 -0300

    webscraping mercado livre

--- scr/coleta/spiders/mercadolivre.py
+++ scr/coleta/spiders/mercadolivre.py
@@ -4,6 +4,8 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+    page_count = 1
+    max_pages = 10
 
     def parse(self, response):
         products = response.css('div.ui-search-result__content') 
@@ -24,3 +26,8 @@ class MercadolivreSpider(scrapy.Spider):
                 'reviews_rating_number': product.css('span.ui-search-reviews__rating-number::text').get(),
                 'reviews_amount': product.css('span.ui-search-reviews__amount::text').get()
             }
+        if self.page_count < self.max_pages:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                self.page_count += 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
